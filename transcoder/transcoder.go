31b8dbbc155750339a7a7aad721baa66cbefa777
Rex Dev <iamrexdev@gmail.com>
2018-07-10 15:50:59 -0700

    built configs for local vars and JSON import

--- transcoder/transcoder.go
+++ transcoder/transcoder.go
@@ -2,123 +2,52 @@ package transcoder
 
 import (
 	"os"
-	"io/ioutil"
-	"encoding/json"
-	"fmt"
+	"os/exec"
 )
 
-
 type Transcoder interface {
-	TransStore(file os.File)(url string, err error)
-	Transcode(file os.File, toMime string)(data byte, err error)
-}
-
-type LocalVars struct {
-	paths map[string] string `json:"paths"`
-}
-
-type Env interface {
-	GetVars() (LocalVars, error)
+	RunToMediaLibrary()(SoundFileMeta, err error)
+	NewJob(file os.File, targetMime []string)(source SoundFileMeta)
+	RunJobs()(data byte, err error)
 	ExitChan() chan error
 }
 
+type TranscodeJob struct {
+	ready bool
+	done bool
+	sourceMeta SoundFileMeta
+	targetMeta SoundFileMeta
+	ffmpegCMD exec.Cmd
+}
+
 type SoundFileMeta struct {
+	id string
 	uri string
 	encoding string
-	codexLib string
+	codex string
 	size int
 }
 
-//type FFMPEG interface {
-//	Trancode(file os.File, meta SoundFileMeta) (os.File)
-//
-//}
+func buildFFMPEGCMD(sourceMeta SoundFileMeta) *exec.Cmd {
+	return exec.Command("ffmpeg", "-i", sourceMeta.uri, "-vn", "-ar 44100", "-ac 2", "-ab 192l", "-f mp3", sourceMeta.id + ".mp3")
+}
 
-func initializeFFMPEG() () {
-	fmt.Print("starting transcoder")
+type TranscoderClient struct {
+	transcodes chan
+}
 
-	// Get ENVIRONMENT VARS
-	EnvPath := "./local.json"
-	localVars := make(map[string]string)
-	local, err := ioutil.ReadFile(EnvPath)
-	if (err != nil) {
-		panic(err)
-	}
-	if err = json.Unmarshal(local, &localVars); err == nil {
-		fmt.Print(localVars)
-	}
+func BuildTranscodeClient() Transcoder {
 
 }
 
-func ProcessEnv(envChan chan Env, envPath string) {
-		for {
-			fmt.Println("\n env!")
-			envVars := <- envChan
-			vars := make(map[string] string)
-			content, err := ioutil.ReadFile(envPath)
-			if err == nil {
-				if err = json.Unmarshal(content, &vars); err == nil {
-					//vals, err := envVars.GetVars()
-					//if err != nil {
-					//	envVars.ExitChan() <- err
-					//}
-					//	fmt.Println("printing Env")
-					//	fmt.Println(vals)
-				}
-
-			}
-			envVars.ExitChan() <- err
-		}
-}
-
-type ReadEnvVars struct {
-	ec chan LocalVars
-	exitChan chan error
-}
-
-func NewReadEnvVars() *ReadEnvVars {
-	return &ReadEnvVars{
-		ec: make(chan LocalVars, 1),
-		exitChan: make(chan error, 1),
-	}
-}
-
-func (ev ReadEnvVars) ExitChan() chan error {
-	return ev.exitChan
-}
-
-func (ev ReadEnvVars) GetVars(localVars LocalVars) (LocalVars, error) {
-
-	ev.ec <- localVars
-
-	// ??
-	return LocalVars{map[string]string{}}, nil
-}
 
 func TransStore() {
-	initializeFFMPEG()
+	//initializeFFMPEG()
 
 	//exec.Command("ffmpeg", )
 	// catch STDOUT
 }
 
 func Transcode() {
-	initializeFFMPEG()
+	//initializeFFMPEG()
 }
-
-func mimeOf(file os.File) {
-
-}
-
-type EnvClient struct {
-	envs chan Env
-}
-
-//func (c *EnvClient) GetLocalVars() ([]LocalVars, error) {
-	//arr := make([]LocalVars, 0)
-	//
-	//localVar := NewReadEnvVars()
-	//c.envs <- localVar
-//
-
-//}
